d186c3f65049625d9ef141f544603038e066cb0e
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-21 10:45:34 -0600

    GetMoneySpent: Test passed

--- Problem Solving/ElectronicShop.js
+++ Problem Solving/ElectronicShop.js
@@ -2,25 +2,17 @@ function getMoneySpent(keyboards, drives, b) {
   /*
    * Write your code here.
    */
-  keyboards.sort();
-  drives.sort();
-  console.log(keyboards, drives);
-  if (keyboards[0] + drives[0] > b) {
-    return -1;
-  } else {
-    let maxVal = 0;
-    let temp = 0;
-    for (let i = keyboards.length - 1; i > -1; i--) {
-      for (let k = drives.length - 1; k > -1; k--) {
-        temp = keyboards[i] + drives[k];
-        console.log(keyboards[i], drives[k]);
-        if (temp <= b && temp > maxVal) {
-          maxVal = temp;
+  let temp = -1;
+  for (let i = 0; i < keyboards.length; i++) {
+    for (let j = 0; j < drives.length; j++) {
+      if (keyboards[i] + drives[j] <= b) {
+        if (keyboards[i] + drives[j] > temp) {
+          temp = keyboards[i] + drives[j];
         }
       }
     }
-    return maxVal;
   }
+  return temp;
 }
 
 console.log(getMoneySpent([3, 1], [5, 2, 8], 10));
