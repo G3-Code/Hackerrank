6ca71d9eb177a8724a583847c54571aabd2fb23d
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-21 09:57:59 -0600

    Completed pageturn

--- Problem Solving/PageTurn.js
+++ Problem Solving/PageTurn.js
@@ -2,7 +2,22 @@ function pageCount(n, p) {
   /*
    * Write your code here.
    */
-  return n;
+  let middle = Math.floor(n / 2);
+  if (p > middle) {
+    turns = Math.floor((n - p) / 2);
+  } else {
+    turns = Math.floor(p / 2);
+  }
+  if (!(n & 1) && p + 1 === n && n > 2) {
+    return 1;
+  }
+  return turns;
 }
 
-console.log(pageCount(5, 4));
+// console.log(pageCount(6, 6));
+// console.log(pageCount(6, 5));
+// console.log(pageCount(6, 4));
+// console.log(pageCount(6, 3));
+// console.log(pageCount(6, 2));
+console.log(pageCount(2, 1));
+console.log(pageCount(7, 4));
