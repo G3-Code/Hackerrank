1f40798c60a99f52f02e84c863041cc82207b296
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-23 22:17:03 -0600

    Hurdle race completed

--- Problem Solving/HurdleRace.js
+++ Problem Solving/HurdleRace.js
@@ -1,6 +1,11 @@
 // Complete the hurdleRace function below.
 function hurdleRace(k, height) {
-  return height;
+  height.sort(function(a, b) {
+    return b - a;
+  });
+  let diff = height[0] - k;
+  if (diff > 0) return diff;
+  else return 0;
 }
 
 console.log(hurdleRace(4, [1, 6, 3, 5, 2]));
