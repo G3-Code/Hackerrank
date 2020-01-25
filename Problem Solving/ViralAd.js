5ae1af55375d6dfb6f43be8ed48feb408343f7a9
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-25 09:50:38 -0600

    ViralAd completed

---
+++ Problem Solving/ViralAd.js
@@ -0,0 +1,10 @@
+function viralAd(n) {
+  let a = 5;
+  let result = 0;
+  for (let i = 0; i < n; i++) {
+    let l = Math.floor(a / 2);
+    result = result + l;
+    a = l * 3;
+  }
+  return result;
+}
