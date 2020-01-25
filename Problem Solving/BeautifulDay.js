83b6d87b15a3db2e7f0644cbd309ff84b45516a7
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-25 08:55:56 -0600

    BeautifulDay solved

---
+++ Problem Solving/BeautifulDay.js
@@ -0,0 +1,22 @@
+function beautifulDay(a, b, c) {
+  //Loop from a to b
+  let temp = a;
+  let resultArr = [];
+  while (temp < b + 1) {
+    let newTemp = parseInt(
+      temp
+        .toString()
+        .split("")
+        .reverse()
+        .toString()
+        .replace(/,/g, "")
+    );
+
+    if (Math.abs(temp - newTemp) % c === 0) {
+      resultArr.push(temp);
+    }
+    temp = temp + 1;
+  }
+  console.log(resultArr);
+  return resultArr.length;
+}
