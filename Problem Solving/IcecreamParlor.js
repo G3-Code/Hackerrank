f6978cfe4bd68d2abcd1f7d5ab8443abea8a97cd
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-02-15 18:20:26 -0600

    Icecream parlor - exercise

--- Problem Solving/IcecreamParlor.js
+++ Problem Solving/IcecreamParlor.js
@@ -1,8 +1,10 @@
 // Complete the icecreamParlor function below.
-export default function icecreamParlor(m, arr) {
+function icecreamParlor(m, arr) {
   for (let i = 0; i < arr.length; i++) {
     if (arr.includes(m - arr[i])) {
-      return [i + 1, arr.findIndex(ar => ar === m - arr[i])];
+      return [i + 1, arr.findIndex(ar => ar === m - arr[i]) + 1];
     }
   }
 }
+
+console.log(icecreamParlor(4, [1, 4, 5, 3, 2]));
