27433d94beb98cbd117bc1d6b2d8d7e2ddfdf72e
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-02-19 18:46:35 -0600

    Icecream parlor - exercise

--- Problem Solving/IcecreamParlor.js
+++ Problem Solving/IcecreamParlor.js
@@ -10,5 +10,6 @@ function icecreamParlor(m, arr) {
 }
 
 //console.log(icecreamParlor(100, [5, 75, 25]));
-console.log(icecreamParlor(200, [150, 24, 79, 50, 88, 345, 3]));
+//console.log(icecreamParlor(200, [150, 24, 79, 50, 88, 345, 3]));
+console.log(icecreamParlor(8, [2, 1, 9, 4, 4, 56, 90, 3]));
 // console.log(icecreamParlor(4, [2, 2, 4, 3]));
